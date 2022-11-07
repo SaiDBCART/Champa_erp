9834327b4735fb1602b3c02e76c8aaf224284d62
iamwillu77 <wilsonam89@gmail.com>
2022-11-07 14:23:20 +0530

    product  and create product

---
+++ src/app/components/pages/dashboard/products/editproduct/editproduct.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/components/commonservice';
+@Component({
+  selector: 'app-editproduct',
+  templateUrl: './editproduct.component.html',
+  styleUrls: ['./editproduct.component.scss']
+})
+export class EditproductComponent implements OnInit {
+  isNavMinimized = false;
+
+  constructor(public commonService: CommonService) { }
+
+  ngOnInit(): void {
+    this.commonService.aClickedEvent
+      .subscribe((data: string) => {
+        this.isNavMinimized = !this.isNavMinimized;
+
+        console.log('Event message from Component A: ' + data);
+      });
+  }
+
+}
--- src/app/components/pages/dashboard/products/products.component.ts
+++ src/app/components/pages/dashboard/products/products.component.ts
@@ -1,6 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonService } from 'src/app/components/commonservice';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+
+export interface PeriodicElement {
+  name: string;
+  reference: string;
+  weight: string;
+  status: string;
+  total: string;
+  paid: string;
+  due: string;
+  warehouse: string;
+  type: string;
+  createdon: string;
+}
+
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { reference: "SA_1111966", name: 'APPLE', weight: "kilogram", warehouse: 'Limestone', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: "SA_1111966", name: 'ORANGE', weight: "kilogram", warehouse: 'Flipup', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'MOBILE', weight: "piece", warehouse: 'worldsbest', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'LAPTOP', weight: "piece", warehouse: 'Wondersstore', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'SUNGLASS', weight: "piece", warehouse: 'Coolindia', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'UMBERLLA', weight: "piece", warehouse: 'NewTown', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'RAINCOAT', weight: "box", warehouse: 'Timesnew', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'T-SHIRT', weight: "pack", warehouse: 'TitanShore', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'KEYBOARD', weight: "piece", warehouse: 'ClassicWorld', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+  { reference: 'SA_1111966', name: 'NOTEBOOK', weight: "pack", warehouse: 'Dell', status: '$500', total: "8665", paid: "yes", type: "cash", due: 'no', createdon: '21/10/2022' },
+];
+
 
 @Component({
   selector: 'app-products',
@@ -9,8 +41,13 @@ import { CommonService } from 'src/app/components/commonservice';
 })
 export class ProductsComponent implements OnInit {
   isNavMinimized = false;
+  ShowModal = false;
+  showpopup = false;
+  displayedColumns: string[] = ['code', 'name', 'brand', 'weight', 'paid', 'type', 'createdon', 'action'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor(public commonService: CommonService, private router: Router) { }
+
+  constructor(public commonService: CommonService, private _liveAnnouncer: LiveAnnouncer, private router: Router) { }
 
   ngOnInit(): void {
     this.commonService.aClickedEvent
@@ -20,12 +57,38 @@ export class ProductsComponent implements OnInit {
         console.log('Event message from Component A: ' + data);
       });
   }
-  viewproduct() {
-    this.router.navigate(["/view-product"])
+  OnproductView() {
+    this.router.navigate(["/viewproduct"])
 
   }
-  editproduct() {
-    this.router.navigate(["/Edit-product"])
+  OnproductEdit() {
+    this.router.navigate(["/editproduct"])
+  }
+  @ViewChild(MatSort) sort: MatSort | any;
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  onfilterOption() {
+    this.showpopup = !this.showpopup;
+  }
+  OnCancelSale() {
+    this.ShowModal = false;
+  }
+  ShowModel() {
+    this.ShowModal = true;
+  }
 }
